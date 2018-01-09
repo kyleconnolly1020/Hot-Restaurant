80bac074432131a12316d73546d0b7b9947c1d58
Kyle <kyleconnolly1020@gmail.com>
2018-01-09 14:11:49 -0800

    init commit

---
+++ server.js
@@ -0,0 +1,70 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var tables = [
+    {
+        name: "Kyle",
+        partyof: 4,
+    },
+    {
+        name: "Ben",
+        partyof: 3,
+    },
+    {
+        name: "Stephen",
+        partyof: 2,
+    },
+];
+
+var waitList = [];
+// Routes
+// =============================================================
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "home.html"))
+});
+
+app.get("/tables", function (req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"))
+});
+
+app.get("/reserve", function (req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"))
+});
+
+// Create New Tables if less than 5 - takes in JSON input
+app.post("/api/new", function (req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body-parser middleware
+    var newParty = req.body;
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    // res.json(newParty);
+    if (tables.length < 5) {
+        tables.push(newParty);
+        console.log("Table is open for the new party");
+        res.json({ message: "You've been added to a table!", data: newParty })
+    }
+
+    else {
+        waitList.push(newParty);
+        console.log("Party was added to waiting list");
+        res.json({ message: "Sorry, you gotta wait.....", data: newParty });
+    }
+    
+
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
