3f389c105cb5aed774ecba2aa8a3335b5e907443
Kyle <kyleconnolly1020@gmail.com>
2018-01-10 11:01:45 -0800

    finished app

--- server.js
+++ server.js
@@ -40,14 +40,16 @@ app.get("/reserve", function (req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"))
 });
 
+app.get("/api", function(req, res){
+    return res.json({"tables": tables, "waitlist": waitList});
+});
+
 // Create New Tables if less than 5 - takes in JSON input
 app.post("/api/new", function (req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body-parser middleware
     var newParty = req.body;
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    // res.json(newParty);
+    
     if (tables.length < 5) {
         tables.push(newParty);
         console.log("Table is open for the new party");
